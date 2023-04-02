b45d0ad280c2a44671ccadccb2e3e7c404287d6b
Jorman Urbina <jormanurbina1@gmail.com>
2023-04-01 21:21:18 -0300

    se agrego plugins para css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import "./styles/global.scss";
 
-ReactDOM.render(<App/>,document.getElementById("app"));
+ReactDOM.createRoot(document.getElementById("app")).render(<App/>);
